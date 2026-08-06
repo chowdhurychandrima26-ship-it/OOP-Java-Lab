8704d487d28c5b05b58a31646e6659d3ff3b2262
chowdhurychandrima26-ship-it <chowdhurychandrima26@gmail.com>
2026-08-06 19:27:53 +0600

    Add files via upload

---
+++ OOP Java Basic Programming Lab/Experiment9.java
@@ -0,0 +1,11 @@
+public class Experiment9 {
+    public static void main(String[] args) {
+
+        double result =
+                ((25.5 * 3.5 - 3.5 * 3.5) /
+                (40.5 - 4.5));
+
+        System.out.println(result);
+
+    }
+}
